6103001ce8f68fd4d5ccf53de7a0e70b231d2eee
megha13 <megha.sinha13@gmail.com>
2019-05-18 23:39:46 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { QuestionFormComponent } from  './question-form/question-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
