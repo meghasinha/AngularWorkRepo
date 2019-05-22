4631ef6db420376dcbd32e9125c2b3e46108bcc2
megha13 <megha.sinha13@gmail.com>
2019-05-22 23:12:51 +0200

    Angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { QuestionFormComponent } from  './question-form/question-form.component';
+import { ResultsComponent } from './results/results.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { QuestionsService } from './questions.service';
+
+
+const appRoutes:Routes=[
+  {path:'welcome', component:WelcomeComponent},
+  {path:':name', component:QuestionsComponent},
+  {path:'',redirectTo:"welcome", pathMatch:"prefix" },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionFormComponent
+    QuestionFormComponent,
+    ResultsComponent,
+    WelcomeComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [QuestionsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
